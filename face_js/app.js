1cb2a77b88e12b72878bd7af7c8b43176521aa73
Joseph Mendieta <jyma1204@gmail.com>
2024-05-02 17:33:53 -0500

    Se le agrego para que solo identificara personas

--- face_js/app.js
+++ face_js/app.js
@@ -26,6 +26,9 @@ function draw() {
   for (let i = 0; i < detections.length; i++) {
     drawResult(detections[i]);
   }
+   console.log('Cantidad de personas detectadas:', personCount);
+
+  //console.log('Cantidad de objetos detectados:', objectCount);
 }
 
 function drawResult(object) {
@@ -83,3 +86,77 @@ function toggleDetecting() {
   }
   detecting = !detecting;
 }
+let personCount = 0;
+
+function onDetected(error, results) {
+  if (error) {
+    console.error(error);
+    return;
+  }
+
+  // Reiniciamos el conteo en cada detección
+  personCount = 0;
+
+  // Filtramos las detecciones para contar solo las personas
+  results.forEach(result => {
+    if (result.label === 'person') {
+      personCount++;
+    }
+  });
+
+  // Filtramos las detecciones para mostrar solo las personas
+  detections = results.filter(result => result.label === 'person');
+
+  if (detecting) {
+    detect();
+  }
+}
+
+
+/*
+let objectCount = 0;
+
+function onDetected(error, results) {
+  if (error) {
+    console.error(error);
+  }
+
+  // Reiniciamos el conteo en cada detección
+  objectCount = 0;
+
+  // Contamos todos los objetos detectados
+  objectCount = results.length;
+
+  detections = results;
+
+  if (detecting) {
+    detect();
+  }
+}
+
+
+
+let personCount = 0;
+
+function onDetected(error, results) {
+  if (error) {
+    console.error(error);
+  }
+
+  // Reiniciamos el conteo en cada detección
+  personCount = 0;
+
+  // Filtramos las detecciones para contar solo las personas
+  results.forEach(result => {
+    if (result.label === 'person') {
+      personCount++;
+    }
+  });
+
+  detections = results;
+
+  if (detecting) {
+    detect();
+  }
+}
+}*/
